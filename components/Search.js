a3e794858f4e8475926f516cb5c81646cf79ba50
Jairo Jair Toro Novellis <jtnovellis88@gmail.com>
2023-01-18 23:08:32 -0500

    add search bar

--- components/Search.js
+++ components/Search.js
@@ -1,9 +1,17 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
+import {
+  AdjustmentsVerticalIcon as AdjustmentsVerticalIconOutline,
+  MagnifyingGlassIcon as MagnifyingGlassIconOutline,
+} from 'react-native-heroicons/outline';
 
 const Search = () => {
   return (
-    <View>
-      <Text>Search</Text>
+    <View className='flex-row items-center space-x-2 pb-2 mx-4'>
+      <View className='flex-row space-x-2 bg-gray-200 p-3 flex-1'>
+        <MagnifyingGlassIconOutline color='gray' size={20} />
+        <TextInput placeholder='Restaurants' keyboardType='default' />
+      </View>
+      <AdjustmentsVerticalIconOutline color='#00CCBB' />
     </View>
   );
 };
